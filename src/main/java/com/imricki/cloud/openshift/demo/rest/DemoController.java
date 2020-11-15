52758e53447b7b85c070e6dd1da95b951848c00f
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-11-15 15:38:04 +0000

    endpoint with parameters

--- src/main/java/com/imricki/cloud/openshift/demo/rest/DemoController.java
+++ src/main/java/com/imricki/cloud/openshift/demo/rest/DemoController.java
@@ -1,14 +1,21 @@
 package com.imricki.cloud.openshift.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+class DemoController {
 
 	@GetMapping("/hi")
 	public String sayHi() {
 		
 		return  "Hi From the Cloud";
+	}
+	
+	@GetMapping("/hi/{name}")
+	public String sayHiByname(@PathVariable String name) {
+		
+		return  "Hi " + name + " from the Cloud";
 	}
 }
